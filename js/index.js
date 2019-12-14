8870ecb348da187c2359d4872f417af33a6e13d3
khanhtc1202 <khanhtc1202@gmail.com>
2019-12-15 02:50:34 +0900

    badpoem

--- js/index.js
+++ js/index.js
@@ -5,4 +5,12 @@ function renderText(src, statusText) {
 document.addEventListener('DOMContentLoaded', async () => {
     let text = "What do want to say?";
     renderText('greeting', text);
+    // click
+    ['goodpoem', 'badpoem', 'normalpoem'].forEach(function(name) {
+    document.getElementById(name).onclick = function() {
+        const audio = new Audio();
+        audio.src = `audio/${name}.mp3`;
+        audio.play();
+      };
+    });
 });
