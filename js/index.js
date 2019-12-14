9967081403607fef51efcba75bf4cdf6bc2fa9e4
khanhtc1202 <khanhtc1202@gmail.com>
2019-12-15 02:57:33 +0900

    change icon, audio from qsona domain

--- js/index.js
+++ js/index.js
@@ -6,10 +6,11 @@ document.addEventListener('DOMContentLoaded', async () => {
     let text = "What do want to say?";
     renderText('greeting', text);
     // click
+    let api = "https://qsona.github.io/goodpoem-button";
     ['goodpoem', 'badpoem', 'normalpoem'].forEach(function(name) {
     document.getElementById(name).onclick = function() {
         const audio = new Audio();
-        audio.src = `audio/${name}.mp3`;
+        audio.src = `${api}/audio/${name}.mp3`;
         audio.play();
       };
     });
