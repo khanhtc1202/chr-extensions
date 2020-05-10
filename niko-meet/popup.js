911a978a1e41c3ab8a9790a2e8dce1062dd9d50d
khanhtc1202 <khanhtc1202@gmail.com>
2020-05-10 09:14:52 +0900

    remove popup script

--- niko-meet/popup.js
+++
@@ -1,9 +0,0 @@
-
-function renderText(dest, text) {
-  document.getElementById(dest).textContent = text;
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-  renderText('title', 'Nico Nico Meet');
-  renderText('description', 'Only works on Google Meet page :)');
-});
